1e8cf36e332f455d8d756c49002b19da07adf99d
Hardeep2401 <hardeep.singh@lpinfotech.com>
2023-01-25 19:44:19 +0530

    initial commit

---
+++ tailindNavbar/src/js/main.js
@@ -0,0 +1,11 @@
+// navbar ul li 
+
+// function myFunction() {
+//     let main = document.getElementById("state");
+//     if (main.style.display === "none") {
+//         main.style.display = "block";
+//     } else {
+//         main.style.display = "none";
+//     }
+// }
+ 
